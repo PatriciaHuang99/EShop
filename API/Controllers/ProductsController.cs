25575799a2bd3fbec121da7d9403090ee311e7e5
WEI-JU Huang <wxh184@cs.bham.ac.uk>
2023-06-19 14:39:26 +0100

    change to async + gitignore

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,6 +1,7 @@
 using API.Data;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -8,21 +9,23 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class ProductsController: ControllerBase
     {
-        private readonly StoreContext context;
+     
+ 
+        private readonly StoreContext _context;
+
         public ProductsController(StoreContext context )
         {
-            this.context = context;
-            
+            _context = context;
         }
         [HttpGet]
-        public ActionResult<List<Product>> GetProducts(){
-            var products = context.Products.ToList();
-            return Ok(products);
+        public async Task<ActionResult<List<Product>>> GetProducts(){
+            return await _context.Products.ToListAsync();
+            
         }
 
         [HttpGet("{id}")] // api/products/3
-        public ActionResult<Product> GetProduct(int id){
-            return context.Products.Find(id);
+        public async Task<ActionResult<Product>> GetProduct(int id){ 
+            return await _context.Products.FindAsync(id);
         }
     }
 }
