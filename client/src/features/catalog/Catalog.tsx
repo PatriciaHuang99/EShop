4a0dcc2a87b0286e2247907b998f57cf8ed09165
WEI-JU Huang <wxh184@cs.bham.ac.uk>
2023-07-03 19:27:48 +0100

    add ProductCard and ProductLisht components

--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -1,6 +1,7 @@
 import { Fragment } from "react";
 import { Product } from "../../app/models/product";
 import { Avatar, Button, List, ListItem, ListItemAvatar, ListItemText } from "@mui/material";
+import ProductList from "./ProductList";
 
 
 // The interface `Props` defines the properties that should be passed to the component.
@@ -14,18 +15,7 @@ interface Props {
 export default function Catalog({products, addProduct}: Props) {
   return (
     <>
-      <List>
-        {products.map((product) => (
-          <ListItem key={product.id}>
-            <ListItemAvatar>
-                <Avatar src={product.pictureUrl}/>
-            </ListItemAvatar>
-            <ListItemText>
-                {product.name} - {product.price}
-            </ListItemText>
-          </ListItem>
-        ))}
-      </List>
+     <ProductList products={products}/>
       <Button variant="contained" onClick={addProduct}>Add Product</Button>
     </>
   );
